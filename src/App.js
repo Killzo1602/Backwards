1766b69cd68cf300f812733a14a6aecace092b63
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-02-02 04:31:01 +0200

    Simple representation of website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import GameComponent from './components/GameComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          //Frangu e smecher
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='notification-message'>
+         <div className='notification-message-text'>
+           <strong className='notification-message-text'>Buy me a beer</strong> 
+            - <a href="localhost:3000" className='notification-message-text'>Patreon</a> ----
+          </div>
+      </section>
+      <div className='header'>
+        <p className='test-text'>SIMPLE TEXT JUST TESTING SOME SHIT WILL CHANGE LATER</p>
+      </div>
+      {/* NEW GAME RELEASE */}
+      <div className='game-row'>
+        <h1 className='row-tags'>NEW GAME RELEASE</h1>
+        <div className='game-list'>
+          <GameComponent></GameComponent>
+          <GameComponent></GameComponent>
+          <GameComponent></GameComponent>
+         </div>
+      </div>
+      {/*PS PLUS GAMES*/}
+      <div className='game-row'>
+        <h1 className='row-tags'>PS PLUS GAMES</h1>
+        <div className='game-list'>
+          <GameComponent></GameComponent>
+          <GameComponent></GameComponent>
+          <GameComponent></GameComponent>
+         </div>
+      </div>
+    </>
   );
 }
+
+
 
 export default App;
--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -1,8 +1,18 @@
 import React from 'react'
-import logo from '../logo.svg';
+import "../components/styles/gamestyles.css"
 
 export default function GameComponent() {
   return (
-    <img src={logo}></img>
+    <div className='game-container'>
+      <div className='game-image'>
+       <img src='../cover.jpg' alt='muie cu ciment'></img>
+      </div>
+
+      <div className='game-details'>
+        <h2>Titlu</h2>
+        <p>Release date</p>
+        <p>Game details</p>
+      </div>
+    </div>
   )
 }
