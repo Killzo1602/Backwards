a81ea2cfec307d9d1e6c910ca23a60942d476fdf
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-02-06 01:51:20 +0200

    passed props

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -2,17 +2,21 @@ import React from "react";
 import logo from "../components/test.jpg";
 import "../components/styles/gamestyles.css";
 
-export default function GameComponent() {
+export default function GameComponent(props) {
+  const gameImage = props.gameImage;
+  const gameName = props.gameName;
+  const releaseDate = props.releaseDate;
+  const gameDetails = props.gameDetails;
   return (
     <div className="game-container">
       <div className="game-image">
-        <img src={logo} alt="test" className="game-img"></img>
+        <img src={gameImage} alt="test" className="game-img"></img>
       </div>
 
       <div className="game-details">
-        <h2>Borderlands 3</h2>
-        <p>Release date: September 13, 2019 </p>
-        <p>Game details</p>
+        <h2>{props.gameName}</h2>
+        <p>Release Date: {props.releaseDate}</p>
+        <p>{props.gameDetails}</p>
       </div>
     </div>
   );
