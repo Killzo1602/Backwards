5580e0a9a97a432ee0c4e32fd2b4f522f6c3ba02
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-02-05 03:38:48 +0200

    Test GameComponent

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import "../components/styles/gamestyles.css"
+import React from "react";
+import logo from "../components/test.jpg";
+import "../components/styles/gamestyles.css";
 
 export default function GameComponent() {
   return (
-    <div className='game-container'>
-      <div className='game-image'>
-       <img src='../cover.jpg' alt='muie cu ciment'></img>
+    <div className="game-container">
+      <div className="game-image">
+        <img src={logo} alt="test" className="game-img"></img>
       </div>
 
-      <div className='game-details'>
-        <h2>Titlu</h2>
-        <p>Release date</p>
+      <div className="game-details">
+        <h2>Borderlands 3</h2>
+        <p>Release date: September 13, 2019 </p>
         <p>Game details</p>
       </div>
     </div>
-  )
+  );
 }
