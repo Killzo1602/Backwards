b33906ef914b2943a760094d38b4f6ff8f71faa0
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-02-06 01:51:41 +0200

    loaded games from pocketbased api

--- src/components/GameLoader.js
+++ src/components/GameLoader.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import GameComponent from "./GameComponent";
 
-export default function GameLoader() {
-    return (
-        // Query from database from games and load them for each game load into gameObject 
-        // For each game object create game component and pass gameobject as props
-    <div>GameLoader</div>
-  )
+export default function GameLoader(props) {
+  const [games, setGames] = useState([""]);
+  useEffect(() => {
+    loadGames().then((value) => setGames(value));
+  }, []);
+
+  const pb = props.pb;
+
+  function loadGames() {
+    const records = pb.collection("games").getFullList();
+    console.log(records);
+
+    return records;
+  }
+
+  return (
+    // Query from database from games and load them for each game load into gameObject
+    // For each game object create game component and pass gameobject as props
+    <>
+      {games.map((game, i) => (
+        <GameComponent
+          gameName={game.name1}
+          releaseDate={game.release_date}
+          gameImage={
+            "http://127.0.0.1:8090/api/files/u30lsz38u8u3l23/" +
+            game.id +
+            "/" +
+            game.game_cover
+          }
+        ></GameComponent>
+      ))}
+    </>
+  );
 }
