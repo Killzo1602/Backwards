dcfbb9e6d65ff9d819fd0cd4db40a9de40777597
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-04-09 04:29:23 +0300

    create /game for games + CSS

---
+++ src/components/gamecomponents/GameInformations.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles/gameinformations.css";
+
+export default function GameInformations() {
+  return (
+    <>
+      <div className="container-location">
+        <div>
+          <div className="game-container-informations">
+            <h2>PS5 Specific</h2>
+            <p>Updated for playstation 5</p>
+            <p>Playstation 5 version</p>
+            <p>HDR</p>
+          </div>
+          <div className="game-container-informations"></div>
+        </div>
+        <div>
+          <div className="game-container-informations"></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
