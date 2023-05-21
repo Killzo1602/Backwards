21d548718121e7bbe0e0960bae817303b4327de6
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-05-21 03:15:22 +0300

    Minor changes

--- src/components/gamecomponents/GameDetail.js
+++ src/components/gamecomponents/GameDetail.js
@@ -3,8 +3,11 @@ import "./styles/gamedetails.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBarsProgress,
+  faDisplay,
+  faDollarSign,
   faGlobe,
   faGun,
+  faUserGear,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function GameDetail() {
@@ -13,23 +16,52 @@ export default function GameDetail() {
       <div className="game-container-informations">
         <h2 className="section-info">Game Details</h2>
         <div className="game-details-box">
-          <div>
+          <div className="game-details-poisiton">
             <p className="game-details-text">
               <FontAwesomeIcon icon={faGun} style={{ marginRight: "5px" }} />
               Genre
             </p>
+            <p className="game-details-text">Developer Name</p>
           </div>
-          <p className="game-details-text">
-            <FontAwesomeIcon
-              icon={faBarsProgress}
-              style={{ marginRight: "5px" }}
-            />
-            Developer
-          </p>
-          <p className="game-details-text">
-            <FontAwesomeIcon icon={faGlobe} style={{ marginRight: "5px" }} />
-            Publisher
-          </p>
+          <div>
+            <p className="game-details-text">
+              <FontAwesomeIcon
+                icon={faUserGear}
+                style={{ marginRight: "5px" }}
+              />
+              Developer
+            </p>
+            <p className="game-details-text">Developer Name</p>
+          </div>
+          <div>
+            <p className="game-details-text">
+              <FontAwesomeIcon icon={faGlobe} style={{ marginRight: "5px" }} />
+              Publisher
+            </p>
+            <p className="game-details-text">Publisher Name</p>
+          </div>
+        </div>
+        <div className="--game-details-box">
+          <div className="game-details-poisiton">
+            <p className="game-details-text">
+              <FontAwesomeIcon
+                icon={faDisplay}
+                style={{ marginRight: "5px" }}
+              />
+              Release date
+            </p>
+            <p className="game-details-text">Date</p>
+          </div>
+          <div className="game-details-poisiton">
+            <p className="game-details-text">
+              <FontAwesomeIcon
+                icon={faDollarSign}
+                style={{ marginRight: "5px" }}
+              />
+              Release date
+            </p>
+            <p className="game-details-text">Price</p>
+          </div>
         </div>
       </div>
     </>
--- src/components/gamecomponents/GameImage.js
+++ src/components/gamecomponents/GameImage.js
@@ -2,6 +2,8 @@ import React from "react";
 import GameplayBlure from "./TestImages/GameCover.png";
 import GameCoverImage from "./TestImages/cover.jpg";
 import ".//styles/gameimage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function GameImage() {
   return (
@@ -13,11 +15,14 @@ export default function GameImage() {
           alt="Gameplay"
           className="gameplay-blure"
         ></img>
-        <img
-          src={GameCoverImage}
-          alt="Game Cover"
-          className="game-cover-image"
-        ></img>
+        <div className="game-image-details">
+          <img
+            src={GameCoverImage}
+            alt="Game Cover"
+            className="game-cover-image"
+          ></img>
+          <h2 className="game-image-text">GAME NAME</h2>
+        </div>
       </div>
     </>
   );
