6c786e53b9db04354c717252953cbe50fedc42be
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-06-11 01:51:10 +0300

    nothing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import PocketBase from "pocketbase";
 import Footer from "./components/Footer";
 import NotificationMessage from "./components/NotificationMessage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import GameList from "./containers/GameList";
 import Game from "./containers/Game";
+
+import PocketBase from "pocketbase";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 
@@ -19,9 +20,10 @@ function App() {
         <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home pb={pb} />}></Route>
-          <Route path="/gamelist" element={<GameList />}></Route>
+          <Route path="/gamelist" element={<GameList pb={pb} />}></Route>
           <Route path="/game" element={<Game />}></Route>
           <Route path="*" element={<NotFound />}></Route>
+          <Route path="/About"></Route>
         </Routes>
       </BrowserRouter>
       <Footer></Footer>
