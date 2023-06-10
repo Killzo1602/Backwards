9832a905fc409d9c7d5abf6b312f1d3712de1c48
Killzo1602 <73487605+Killzo1602@users.noreply.github.com>
2023-06-11 01:51:40 +0300

    Nothing done

--- src/components/gamecomponents/GameDetail.js
+++ src/components/gamecomponents/GameDetail.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./styles/gamedetails.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBarsProgress,
   faDisplay,
   faDollarSign,
   faGlobe,
