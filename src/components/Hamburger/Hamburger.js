9d183478a35778348bc8eef5712b5bbf9973702f
Frann <polarfrann@gmail.com>
2023-06-22 23:50:49 +0300

    added click listener

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style/Hamburger.css";
+import { click } from "@testing-library/user-event/dist/click";
 
 export default function Hamburger(props) {
+
+  const [clicked, setClicked] = useState(false);
+
+
   return (
-    <div className="hamburger-menu">
+    <div className={clicked ? "hamburger-menu clicked" : "hamburger-menu"} onClick={() => setClicked(!clicked)}>
       <div id="first-line" className="line"></div>
       <div id="second-line" className="line"></div>
     </div>
